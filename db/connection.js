6e5d5b3fb04d5c91ee700cc618e907d8c3e11074
3b8ny <ahmedroute5@gmail.com>
2024-08-16 15:05:22 +0300

    asa

--- db/connection.js
+++ db/connection.js
@@ -2,9 +2,9 @@ import mongoose from "mongoose";
 
 
 const connectionDB = async () => {
-    return await mongoose.connect(process.env.DB_URL)
+    return await mongoose.connect(process.env.EcommerceC42Sat)
         .then(() => {
-            console.log(`connected to database on ${process.env.DB_URL}`)
+            console.log(`connected to database on ${process.env.EcommerceC42Sat}`)
         }).catch((err) => {
             console.log({ msg: "fail to connect", err })
         })
